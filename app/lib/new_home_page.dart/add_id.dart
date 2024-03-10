5e016981357b6c9a78045fc74f44a61a6d367af4
qKitNp <pranjal19@pm.me>
2024-03-10 14:41:22 +0530

    newScreens added

---
+++ app/lib/new_home_page.dart/add_id.dart
@@ -0,0 +1,87 @@
+import 'package:app/theme/pallete.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class AddIDPage extends StatelessWidget {
+  const AddIDPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "proto.id",
+            style: TextStyle(fontSize: 32),
+          ),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(0),
+            child: Container(
+              color: Colors.white,
+              height: 1,
+            ),
+          ),
+          bottomOpacity: 1,
+          elevation: 0,
+        ),
+        body: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Spacer(),
+              Text(
+                "register id",
+                style: TextStyle(fontSize: 32),
+              ),
+              Gap(16),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(0)),
+                  ),
+                  // labelText: "unique identifier",
+                  hintText: "unique identifier",
+                ),
+              ),
+              Gap(16),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(0)),
+                  ),
+                  // labelText: "unique identifier",
+                  hintText: "otp",
+                ),
+              ),
+              Gap(8),
+              Text(
+                "retry in 5s",
+                style: TextStyle(fontSize: 16),
+              ),
+              GestureDetector(
+                onTap: null,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    // borderRadius: BorderRadius.cir cular(16),
+                    color: Pallete.whiteColor,
+                  ),
+                  margin: const EdgeInsets.all(16.0),
+                  child: const Center(
+                    child: Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        'submit',
+                        style:
+                            TextStyle(fontSize: 24, color: Pallete.blackColor),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Spacer(),
+            ],
+          ),
+        )));
+  }
+}
---
+++ app/lib/new_home_page.dart/your_id_page.dart
@@ -0,0 +1,86 @@
+import 'package:app/theme/pallete.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class YourIdPage extends StatelessWidget {
+  const YourIdPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "proto.id",
+          style: TextStyle(fontSize: 32),
+        ),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(0),
+          child: Container(
+            color: Colors.white,
+            height: 1,
+          ),
+        ),
+        bottomOpacity: 1,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Text(
+              "your ids",
+              style: TextStyle(fontSize: 32),
+            ),
+            Expanded(
+                child: ListView.builder(
+              physics: const RangeMaintainingScrollPhysics(),
+              itemCount: 3, // Number of items in the list
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    // borderRadius: BorderRadius.circular(16),
+                    color: Pallete.blackColor,
+                    border: Border.all(color: Pallete.whiteColor),
+                  ),
+                  margin: const EdgeInsets.all(16.0),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        'id ${index + 1}',
+                        style: const TextStyle(
+                            fontSize: 24, color: Pallete.whiteColor),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            )),
+            GestureDetector(
+              onTap: null,
+              child: Container(
+                decoration: const BoxDecoration(
+                  // borderRadius: BorderRadius.circular(16),
+                  color: Pallete.whiteColor,
+                ),
+                margin: const EdgeInsets.all(16.0),
+                child: const Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      'add id',
+                      style: TextStyle(fontSize: 24, color: Pallete.blackColor),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
